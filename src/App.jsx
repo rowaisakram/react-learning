ba07f510536f73dc731a52bc0dd094405574efe3
Owais <r.owaisakram@gmail.com>
2024-06-27 18:35:07 +0500

    Navigation added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import Products from "./pages/Products";
+import { Routes, Route } from "react-router-dom";
 import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
 const App = () => {
   return (
     <>
-      {/* <Products /> */}
-      <Signup />
-      <Signin />
+      <Routes>
+        <Route path="/" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/products" element={<Products />} />
+      </Routes>
     </>
   );
 };
